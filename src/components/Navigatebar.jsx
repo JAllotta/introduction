4f5e5dd28b328e0de4a76ceb6bf87c33e37c2dad
JAllotta <jallotta64@gmail.com>
2020-05-24 20:26:47 -0700

    Fixed missing reference in navigatebar.jsx

--- src/components/Navigatebar.jsx
+++ src/components/Navigatebar.jsx
@@ -1,43 +1,21 @@
 import React from 'react';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export const Navigatebar = () => {
     return(
-        <div>
-            {/* <h1>My Site</h1>
-            <div>
-                <A href="/">Home</A>
-                <A href="/about">About</A>
-                <A href="/contact/1234">Contact</A>
-            </div> */}
-
-            
+        <div>            
             <Navbar bg="light" expand="lg">
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/About">About</Nav.Link>
-                <Nav.Link href="/Career">Career</Nav.Link>
-                <Nav.Link href="/contact">Contact</Nav.Link>
-                {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown> */}
-                </Nav>
-                <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-success">Search</Button>
-                </Form>
-            </Navbar.Collapse>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/About">About</Nav.Link>
+                    <Nav.Link href="/Career">Career</Nav.Link>
+                    <Nav.Link href="/contact">Contact</Nav.Link>             
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>        
         </div>
     )
